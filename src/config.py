c1aa955a198567572eb98e3c20d3bf0455a0e5c7
Ryan Marvin <marvinryan@ymail.com>
2018-10-21 17:35:46 +0300

    Add config.py views.py files

---
+++ src/config.py
@@ -0,0 +1,11 @@
+import json
+import os
+
+
+if 'SERVERTYPE' in os.environ and os.environ['SERVERTYPE'] == 'AWS Lambda':
+    json_data = open('zappa_settings.json')
+    env_vars = json.load(json_data)['dev']['environment_variables']
+    for key, val in env_vars.items():
+        os.environ[key] = val
+
+WEATHER_API_KEY = os.environ.get('WEATHER_API_KEY')
